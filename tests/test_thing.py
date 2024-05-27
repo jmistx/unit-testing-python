52390da1fdb99290d4f216a27cfd71a52cbb3060
Oleg Blokhin <o.blokhin@vk.team>
2024-05-27 03:30:37 +0300

    Initial commit

---
+++ tests/test_thing.py
@@ -0,0 +1,12 @@
+# pylint: disable=no-member
+from app.thing import Thing
+
+
+def test_correct_greeting():
+    thing = Thing("Bob")
+    assert "Hello Bob!" == thing.return_hello_name()
+
+
+def test_fail():
+    thing = Thing("Albert")
+    assert "Wrong!" == thing.return_hello_name()
---
+++ tests/test_thing_fixture.py
@@ -0,0 +1,16 @@
+# pylint: disable=no-member
+import pytest
+from app.thing import Thing
+
+
+@pytest.fixture
+def thing():
+    return Thing("Bob")
+
+
+def test_correct_greeting(thing):
+    assert "Hello Bob!" == thing.return_hello_name()
+
+
+def test_fail(thing):
+    assert "Wrong!" == thing.return_hello_name()
